3b04dd074b31e520a01634689da9e26808476158
AdielTuson <adielt81@gmail.com>
2024-06-02 19:47:08 +0300

    Add logo to header

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ console.log('restaurant');
 import loadHome from "./load-home";
 import loadMenu from "./load-menu";
 import loadAbout from "./load-about";
+import './styles/main.css';
+import restaurantLogo from './assets/restaurant-logo.png';
 
-loadHome();
-// loadMenu();
 
 function renderPage() {
     const homeBtn = document.querySelector(".home");
     const menuBtn = document.querySelector(".menu");
     const aboutBtn = document.querySelector(".about");
     const content = document.querySelector("#content");
+    
+    loadLogo();
+    loadHome();
 
     homeBtn.addEventListener("click", () => {
         content.innerHTML = '';
@@ -28,3 +31,8 @@ function renderPage() {
     });
 }
 renderPage();
+
+function loadLogo() {
+    const logo = document.querySelector(".logo");
+    logo.src = restaurantLogo;
+}
