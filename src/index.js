10dd6aad950a452392b29eb827df8342afeff5b1
AdielTuson <adielt81@gmail.com>
2024-06-02 17:22:15 +0300

    Add a function to render page in index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import loadHome from "./load-home";
 import loadMenu from "./load-menu";
 
 loadHome();
-loadMenu();
+// loadMenu();
 
 function renderPage() {
     const homeBtn = document.querySelector(".home");
     const menuBtn = document.querySelector(".menu");
     const aboutBtn = document.querySelector(".about");
+    const content = document.querySelector("#content");
 
     homeBtn.addEventListener("click", () => {
+        content.innerHTML = '';
+        loadHome();
+    });
 
-    })
+    menuBtn.addEventListener("click", () => {
+        content.innerHTML = '';
+        loadMenu();
+    });
+
+    aboutBtn.addEventListener("click", () => {
+        content.innerHTML = '';
+        loadAbout();
+    });
 }
-
+renderPage();
