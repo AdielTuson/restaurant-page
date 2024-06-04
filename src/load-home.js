6c3ee779b709a774bdac7a2d3b93bcc2a0fe2b1b
AdielTuson <adielt81@gmail.com>
2024-06-04 19:21:07 +0300

    Add style to home text and header buttons

--- src/load-home.js
+++ src/load-home.js
@@ -7,13 +7,15 @@ export default function loadHome() {
     const pageImg = document.createElement('img');
     const pageText = document.createElement('p');
 
+    header.classList.add("home-header");
     pageImg.classList.add('home-image');
+    pageText.classList.add('home-text');
 
-    header.textContent = "Adiel's Restaurant";
+    header.textContent = "Adiel's Restaurant, Where Every Meal is a Masterpiece";
     pageImg.src = restaurantImage;
-    pageText.textContent = "In a lovely spot on the bank of 'dodo-lake' lies a homey and lovely restaurant. In our restaurant you can find a lot of special dishes, including the famous 'Adiel's Havita'.";
+    pageText.textContent = "In a lovely spot on the bank of 'dodo-lake' lies a homey and lovely restaurant. In our restaurant you can find a lot of special dishes, including the famous 'Adiel's Havita'. In a lovely spot on the bank of 'dodo-lake' lies a homey and lovely restaurant. In our restaurant you can find a lot of special dishes, including the famous 'Adiel's Havita'.";
 
-    // content.appendChild(header);
+    content.appendChild(header);
     content.appendChild(pageImg);
     content.appendChild(pageText);
 }
