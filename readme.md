ebdbb1475cb37385f9fc1424124682af854d279a
AdielTuson <adielt81@gmail.com>
2024-06-09 17:46:55 +0300

    Finish menu style
    I added a menu div that will store all menu elements instead
    of appending them straight to content div. Added a menu
    header and text dividers. Added dish image box shadow.

--- readme.md
+++ readme.md
@@ -11,6 +11,13 @@ Credits:
 
 - bar Photo by <a href="https://unsplash.com/@zerotake?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">zero take</a> on <a href="https://unsplash.com/photos/brown-wooden-shelf-with-bottles-2zimLZ7aDOM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
   
-  - 
+- salmon Photo by freepik
+
+- potato Photo by <a href="https://unsplash.com/@_carolineattwood?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Caroline Attwood</a> on <a href="https://unsplash.com/photos/grilled-fish-cooked-vegetables-and-fork-on-plate-bpPTlXWTOvg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- sushi Photo by <a href="https://unsplash.com/@deram31?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Riccardo Bergamini</a> on <a href="https://unsplash.com/photos/food-photography-of-varieties-of-sushies-O2yNzXdqOu0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- menu divider by <a href="https://www.freepnglogos.com/images/divider-36054.html">Get it on divider wip digital art cole sprouse complete post steemit</a>
+  
   
   
--- src/load-menu.js
+++ src/load-menu.js
@@ -1,6 +1,41 @@
+import steakImgSrc from './assets/steak.jpg';
+import hamburgerImgSrc from './assets/hamburger.jpg';
+import salmonImgSrc from './assets/salmon.jpg';
+import potatoImgSrc from './assets/potato.jpg';
+import sushiImgSrc from './assets/sushi.jpg';
+import dividerImgSrc from './assets/menu-divider.png';
+
 export default function loadMenu() {
     const content = document.querySelector('#content');
-    content.append(createDishElement(potato));
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+
+    menu.append(
+        createMenuHeader(),
+        createMenuDivider(),
+        createDishElement(potato), 
+        createDishElement(steak),
+        createDishElement(hamburger),
+        createDishElement(salmon),
+        createDishElement(sushi),
+        createMenuDivider(),
+    );
+    content.appendChild(menu);
+
+}
+
+function createMenuHeader() {
+    const header = document.createElement('h1');
+    header.textContent = 'Our Menu';
+    header.classList.add('menu-header');
+    return header;
+}
+
+function createMenuDivider() {
+    const divider = document.createElement('img');
+    divider.src = dividerImgSrc;
+    divider.classList.add('divider');
+    return divider;
 }
 
 function createDishElement(dish) {
@@ -25,4 +60,13 @@ function createDish(imgSrc, name, description) {
     return { imgSrc, name, description }
 }
 
-const potato = createDish("Potato.com", "Potato", "This is the best potato in the world! in fact it is so great you will eat 100000. it comes with hdhd owie fdhoeinf oiend. kkk snisnis didnidneeo dind");
+const potato = createDish(potatoImgSrc, "Roasted Potatoes", "Crispy on the outside and tender on the inside, our Roasted Potatoes are seasoned with aromatic herbs and spices. The perfect complement to any main course.");
+
+const steak = createDish(steakImgSrc, "Premium rib-eye steak", "Succulent, tender, and richly marbled, our Rib-Eye Steak is expertly grilled to perfection. Served with gourmet sauces, it's a luxurious treat for true meat lovers.");
+
+const hamburger = createDish(hamburgerImgSrc, "Adiel's Burger", "Juicy, flavorful, and perfectly seasoned, our Hamburger is grilled to perfection. Served with fresh toppings and gourmet sauces, it's a classic delight for burger lovers");
+
+const salmon = createDish(salmonImgSrc, "Grilled Salmon", "Perfectly grilled and tender, our Salmon is seasoned with a blend of herbs and spices. Served with a side of fresh vegetables, it's a delightful and healthy option for seafood enthusiasts.");
+
+const sushi = createDish(sushiImgSrc, "Sushi Tray", "A beautifully arranged selection of fresh, high-quality sushi, including nigiri, sashimi, and rolls. Perfectly balanced flavors and textures, crafted to delight sushi enthusiasts.");
+
