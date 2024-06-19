fc88aec69f6111d7dd374f5352083f9cf392450a
AdielTuson <adielt81@gmail.com>
2024-06-19 20:54:09 +0300

    Change webpack config files
    Prod and dev use different CSS loaders. Solved the
    site not loading properly with the CSS minimizing
    loader due to the conflict with CSS style loader.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,31 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
     performance: {
         hints: false,
         maxEntrypointSize: 512000,
         maxAssetSize: 512000
     },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins : [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ]
 })
